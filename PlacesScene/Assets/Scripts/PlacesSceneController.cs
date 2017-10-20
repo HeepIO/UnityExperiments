7d98e56f3e74856d09e67cef448d7d0226ba979c
Jacob Keith <jacobkeith42@gmail.com>
2017-10-20 16:29:18 -0400

    Adding generic starter Place Prefab

---
+++ PlacesScene/Assets/Scripts/PlacesSceneController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacesSceneController : MonoBehaviour {
+
+	private List<Place> places = new List<Place>();
+
+	void Update() {
+
+		CheckForNewPlaces ();
+	}
+
+	void CheckForNewPlaces() {
+		if (GlobalData.store.places.Count > places.Count) {
+			places = GlobalData.store.places;
+			Debug.Log ("I want to generate " + places.Count + " places");
+
+		}
+	}
+
+	void DestroyAllExistingPlaces() {
+
+	}
+
+}
