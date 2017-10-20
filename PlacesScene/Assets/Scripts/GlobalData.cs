0403bc5f9e7f3db6f1a339b3172ae81e3b357cc1
Jacob Keith <jacobkeith42@gmail.com>
2017-10-20 14:07:51 -0400

    Adding PLaceScene experiment

---
+++ PlacesScene/Assets/Scripts/GlobalData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalData : MonoBehaviour {
+
+	public static GlobalData store;
+
+	public int health = 100;
+	public int experience = 10;
+
+
+	void Awake () {
+		
+		if (store == null) {
+			DontDestroyOnLoad (gameObject);
+			store = this;
+		} else if (store != this) {
+			Destroy (gameObject);
+		}
+
+	}
+
+	void OnGUI() {
+		GUI.Label (new Rect (10, 10, 100, 30), "Health: " + health);
+		GUI.Label (new Rect (10, 40, 150, 30), "Experience: " + experience);
+	}
+}
---
+++ PlacesScene/Assets/Scripts/LoginToFirebase.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+
+public class LoginToFirebase : MonoBehaviour {
+
+	Firebase.Auth.FirebaseAuth auth;
+	Firebase.Auth.FirebaseUser user;
+
+	void Start() {
+		// Set this before calling into the realtime database.
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://heep-3cddb.firebaseio.com/");
+		FirebaseApp.DefaultInstance.SetEditorP12FileName(Application.dataPath + "/FirebaseAssetPackage/Editor Default Resources/Heep-ede80bde542a.p12");
+		FirebaseApp.DefaultInstance.SetEditorServiceAccountEmail("unity-user@heep-3cddb.iam.gserviceaccount.com");
+		FirebaseApp.DefaultInstance.SetEditorP12Password("notasecret");
+		Debug.Log ("Using P12 for Firebase Access in Editor");
+
+		InitializeFirebase ();
+		ReadDatabase ();
+
+	}
+
+
+	void InitializeFirebase() {
+		Debug.Log("Setting up Firebase Auth");
+		auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+		auth.StateChanged += AuthStateChanged;
+		AuthStateChanged(this, null);
+	}
+
+	void ReadDatabase() {
+
+		DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+		Debug.Log ("Reading...hello");
+
+		FirebaseDatabase.DefaultInstance
+			.GetReference ("hello")
+			.GetValueAsync().ContinueWith(task => {
+
+				GlobalData.store.health += 200;
+
+				if (task.IsFaulted) {
+					// Handle the error...
+					Debug.Log("error");
+				}
+				else if (task.IsCompleted) {
+					DataSnapshot snapshot = task.Result;
+
+					var databaseItems = snapshot.Value as Dictionary<string, object>;
+
+					foreach (var item in databaseItems)
+					{
+						Debug.Log(item.Key); 
+
+						var values = item.Value as Dictionary<string, object>;
+						foreach (var v in values)
+						{
+							Debug.Log(v.Key + ": " + v.Value); 
+						}
+					}
+				}
+			});
+	}
+
+	void AuthStateChanged(object sender, System.EventArgs eventArgs) {
+		if (auth.CurrentUser != user) {
+			
+			bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null;
+
+			if (!signedIn && user != null) {
+				Debug.Log("Signed out " + user.UserId);
+			}
+			user = auth.CurrentUser;
+
+			if (signedIn) {
+				Debug.Log("Signed in " + user.UserId);
+			}
+		}
+	}
+
+}
