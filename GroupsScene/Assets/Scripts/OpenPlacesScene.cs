a0d310e5a15d3e72f37c84b9bc3e52183bf6829f
KacobJeith <jacobkeith42@gmail.com>
2017-10-21 13:15:26 -0400

    Adding button to navigate back to places scene from group scene

---
+++ GroupsScene/Assets/Scripts/OpenPlacesScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OpenPlacesScene : MonoBehaviour {
+
+	public Button back;
+
+	void Start()
+	{
+		Button btn = back.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		SceneManager.LoadScene ("PlacesScene");
+	}
+
+}
